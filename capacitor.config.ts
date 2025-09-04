9197d2d0368d6a0e64e768c3adcacf3120f4a83b
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-04 11:12:48 +0200

    ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'absent-altitude',
-  webDir: 'dist'
+  webDir: 'dist',
+  "server":{
+    "androidScheme": "https",
+    "url": "http://192.168.1.39:4321",
+    "cleartext": true
+  }
 };
 
 export default config;
